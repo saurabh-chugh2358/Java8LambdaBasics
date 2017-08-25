9b4c2d9b182a82cd14e512c36466d2effc6812ce
saurabh-chugh2358 <saurabh08988@gmail.com>
2017-08-24 19:56:38 -0700

    Passing a behavior using an Interface, Anonymous Inner Class & Lamda Expression

--- src/Greeter.java
+++ src/Greeter.java
@@ -3,9 +3,26 @@ public class Greeter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Greeter greeter = new Greeter();
+		Greeter greeter1 = new Greeter();
+
+		// Passing a behavior using an interface
 		HelloWorldGreeting greeting = new HelloWorldGreeting();
-		greeter.greet(greeting);
+		greeter1.greet(greeting);
+		
+
+		// Passing a behavior using an Anonymous Inner Class
+		Greeting innerClassLamda = new Greeting() {
+			@Override
+			public void perform() {
+				// TODO Auto-generated method stub
+				System.out.println("Hello Inner Class World...!!!");
+			}
+		};
+		greeter1.greet(innerClassLamda);
+		
+		// Passing a behavior using an Anonymous Lamda Expression
+		Greeting helloLamdaExpression = () -> System.out.println("Hello Lamda World...!!!");
+		greeter1.greet(helloLamdaExpression);
 	}
 	
 	public void greet(Greeting greet) {
