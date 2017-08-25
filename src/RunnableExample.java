59d94a5c8e584d2088a263639543eb08911e078f
saurabh-chugh2358 <saurabh08988@gmail.com>
2017-08-25 15:58:55 -0700

    Anonymous Class Implementation & Lambda Expression Implementation of Runnable in Threads

---
+++ src/RunnableExample.java
@@ -0,0 +1,21 @@
+
+public class RunnableExample {
+
+	public static void main(String[] args) {
+		// Anonymous Runnable Class Implementation of Runnable object.
+		Thread myThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Printed Inside Anonymous Runnable Class");
+			}
+		});
+		
+		myThread.start();
+		
+		// Lambda Expression implementation of Runnable object.
+		Thread myLambdaThread = new Thread(() -> System.out.println("Printed Inside Lambda Runnable"));
+		myLambdaThread.start();
+	}
+
+}
