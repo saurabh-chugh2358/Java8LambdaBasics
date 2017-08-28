259d9323366f7f2f5e48b57f016eb6ce4ef83ed5
saurabh-chugh2358 <saurabh08988@gmail.com>
2017-08-27 20:13:43 -0400

    Exception Handling using a wrapper method for lambda expressions

--- src/unit2Exercise/exceptionHandling/ExceptionHandlingExample.java
+++ src/unit2Exercise/exceptionHandling/ExceptionHandlingExample.java
@@ -8,18 +8,22 @@ public class ExceptionHandlingExample {
 		int[] numbers = {1,2,3,4};
 		int key = 2;
 		
-		process(numbers, key, (v, k)->{
-			try {
-				System.out.println(v/(v-k));
-			} catch (ArithmeticException e) {
-				System.out.println("An Arithmetic Exception Happenned...!");
-			}
-		});
+		process(numbers, key, wrapperLambda((v, k)->System.out.println(v/(v-k))));
 	}
 
-	public static void process(int[] numbers, int key, BiConsumer<Integer, Integer> consumer) {
+	private static void process(int[] numbers, int key, BiConsumer<Integer, Integer> consumer) {
 		for (int i = 0; i < numbers.length; i++) {
 			consumer.accept(numbers[i], key);
 		}
+	}
+	
+	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer) {
+		return (v, k)->{
+			try {
+				consumer.accept(v, k);
+			} catch (ArithmeticException e) {
+				System.out.println("Try-Catch Inside Wrapper Lambda Function");
+			}
+		};
 	}
 }
