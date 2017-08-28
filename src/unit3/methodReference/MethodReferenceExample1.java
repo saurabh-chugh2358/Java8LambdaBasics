b4ee37418cfde9844fb1b946a67999c86592780d
saurabh-chugh2358 <saurabh08988@gmail.com>
2017-08-28 00:18:59 -0400

    Lamda Expression Syntax for Method Execution

---
+++ src/unit3/methodReference/MethodReferenceExample1.java
@@ -0,0 +1,19 @@
+package unit3.methodReference;
+
+public class MethodReferenceExample1 {
+
+	public static void main(String[] args) {
+		Thread th1 = new Thread(() -> printMessage());
+		th1.start();
+		
+		// This is equivalent to the lambda expression in above statement
+		// MethodReferenceExample1::printMessage === () -> printMessage()
+		Thread th2 = new Thread(MethodReferenceExample1::printMessage);
+		th2.start();
+	}
+	
+	public static void printMessage() {
+		System.out.println("Hello");
+	}
+
+}
